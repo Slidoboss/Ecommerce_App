ff653075f9d39c2e10cfe1e2abede41b7b5adce1
Slidoboss <usoloudo@gmail.com>
2025-04-15 15:29:58 +0100

    Updated my comment.

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,4 +1,4 @@
 abstract class UseCase<Type,Params> {
   
-  Future<Type> call({Params params}); //Params is a type that allows you define params with any DataType.
+  Future<Type> call({Params params}); //Params is a type that allows you define params with any DataType. Type as well does the same thing.
 }
