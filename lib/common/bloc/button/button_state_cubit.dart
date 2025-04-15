35cdee79e97f3c16ad6e6b0a83ea7d89b6686dbd
Slidoboss <usoloudo@gmail.com>
2025-04-15 15:30:58 +0100

    Added new cubit class for reactive button.

---
+++ lib/common/bloc/button/button_state_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/common/bloc/button/button_state.dart';
+import 'package:ecommerce_app/core/usecase/usecase.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ButtonStateCubit extends Cubit<ButtonState> {
+  ButtonStateCubit() : super(ButtonIntialState());
+
+  Future<void> execute(dynamic params, UseCase usecase) async {
+    emit(ButtonLoadingState());
+    try {
+      Either returnedData = await usecase.call(params: params);
+      returnedData.fold((error) {
+        emit(ButtonFailureState());
+      }, (data) {
+        emit(ButtonSuccessState());
+      });
+    } catch (e) {
+      emit(ButtonFailureState());
+    }
+  }
+}
