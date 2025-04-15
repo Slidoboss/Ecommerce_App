6f31925c7181d8b2dbf7dd78bb64dd4d0e2c772f
Slidoboss <usoloudo@gmail.com>
2025-04-15 13:14:21 +0100

    Created button states

---
+++ lib/common/bloc/button/button_state.dart
@@ -0,0 +1,9 @@
+abstract class ButtonState {}
+
+class ButtonIntialState extends ButtonState {}
+
+class ButtonLoadingState extends ButtonState {}
+
+class ButtonSuccessState extends ButtonState {}
+
+class ButtonFailureState extends ButtonState {}
